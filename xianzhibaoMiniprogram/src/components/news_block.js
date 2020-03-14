39f1050590c79000c6f4e4689898233535256e4a
proger <progerchai@qq.com>
2020-03-14 23:49:38 +0800

    add a component news_block for active

---
+++ xianzhibaoMiniprogram/src/components/news_block.js
@@ -0,0 +1,26 @@
+/*
+ *@description: news_block 组件
+ *@author: progerchai
+ *@date: 2020-03-07 15:05:29
+ */
+import { Component } from "@tarojs/taro";
+import "../assets/styles/news_block.scss";
+import { View, Text, Image } from "@tarojs/components";
+export default class FqBlock extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {};
+
+  render() {
+    let { active_url, active_intr_text, active_img_url } = this.props.item;
+    return (
+      <View className="menu_box">
+        <View className="menu_box_content">
+          <Image className="menu_box_content_img" src={active_img_url}></Image>
+        </View>
+      </View>
+    );
+  }
+}
--- xianzhibaoMiniprogram/src/pages/activity/activity.tsx
+++ xianzhibaoMiniprogram/src/pages/activity/activity.tsx
@@ -3,6 +3,7 @@ import { View, Button, Text } from "@tarojs/components";
 // import { connect } from '@tarojs/redux'
 import FqBottom from "../../components/bottom";
 import FqScrollNotice from "../../components/scroll_notice";
+import FqBlock from "../../components/news_block";
 
 // import { add, minus, asyncAdd } from '../../actions/counter'
 
@@ -12,7 +13,26 @@ class Activity extends Component {
   config = {
     navigationBarTitleText: "活动"
   };
-  state = {};
+  state = {
+    active_list: [
+      {
+        active_id: 1,
+        active_weight: 0,
+        active_intr_text: "快用你的能量来种树吧！",
+        active_img_url:
+          "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4183863574,3435833752&fm=26&gp=0.jpg",
+        active_url: "https://mp.weixin.qq.com/s/N9LIa0BzLse-b5jSciM3OQ"
+      },
+      {
+        active_id: 2,
+        active_weight: 0,
+        active_intr_text: "我爱你，也爱这身白衣！",
+        active_img_url:
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1584208109413&di=9d4455cd2dfcff36834ca7d9d8add5a7&imgtype=0&src=http%3A%2F%2Fpic.anhuinews.com%2F003%2F010%2F876%2F00301087613_bd13fd4a.jpg",
+        active_url: "https://mp.weixin.qq.com/s/er5ID3_UbWwOdNc5jfjIbA"
+      }
+    ]
+  };
   componentWillReceiveProps(nextProps) {
     console.log(this.props, nextProps);
   }
@@ -24,6 +44,7 @@ class Activity extends Component {
   componentDidHide() {}
 
   render() {
+    var { active_list } = this.state;
     return (
       <View className="container">
         <FqScrollNotice
@@ -31,6 +52,9 @@ class Activity extends Component {
           bgColor="#fffbe6"
           fontColor="#6d511e"
         ></FqScrollNotice>
+        {active_list.map(item => {
+          return <FqBlock item={item}></FqBlock>;
+        })}
         <FqBottom current={1}></FqBottom>
       </View>
     );
